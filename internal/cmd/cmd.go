4d20e3d509dbd3c4d23e87cb0acc0e43031fc850
hwipl <33433250+hwipl@users.noreply.github.com>
2022-06-09 09:23:06 +0200

    add dummy topology for testing
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,5 +1,34 @@
 package cmd
 
+import (
+	"fmt"
+)
+
 // Run is the main entry point
 func Run() {
+	// create dummy topology for testing
+	topo := NewTopology()
+
+	node1 := NewNode()
+	node2 := NewNode()
+	node3 := NewNode()
+
+	topo.AddNode(node1)
+	topo.AddNode(node2)
+	topo.AddNode(node3)
+
+	link1 := NewLink()
+	link2 := NewLink()
+
+	link1.Nodes[0] = node1
+	link1.Nodes[1] = node2
+
+	link2.Nodes[0] = node2
+	link2.Nodes[1] = node3
+
+	topo.AddLink(link1)
+	topo.AddLink(link2)
+
+	// dump yaml of topology
+	fmt.Printf("%s", topo.YAML())
 }
