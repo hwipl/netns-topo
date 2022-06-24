ffd9235376be918c29597b9b2b3d34afd5b6cca9
hwipl <33433250+hwipl@users.noreply.github.com>
2022-06-24 17:17:41 +0200

    add netns creation
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/deploy/netns.go
+++ internal/deploy/netns.go
@@ -1,5 +1,17 @@
 package deploy
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+const (
+	// netnsPrefix is the prefix used to build namespace names
+	netnsPrefix = "netns-topo-"
+)
+
 // Netns is a network namespace
 type Netns struct {
 	Name string
@@ -7,6 +19,13 @@ type Netns struct {
 
 // Start starts the network namespace
 func (n *Netns) Start() {
+	name := fmt.Sprintf("%s%s", netnsPrefix, n.Name)
+	cmd := exec.Command("ip", "netns", "add", name)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Println(err)
+	}
 }
 
 // Stop stops the network namespace
