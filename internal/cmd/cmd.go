b7046c3360afdafbf8fc0ee1b5fb378001a3aec1
hwipl <33433250+hwipl@users.noreply.github.com>
2022-07-01 12:05:22 +0200

    add command line arguments
    Add command line arguments for starting and stopping topologies.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,5 +1,41 @@
 package cmd
 
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/hwipl/netns-topo/internal/deploy"
+	"github.com/hwipl/netns-topo/internal/topo"
+)
+
+// parseTopology parses topology from file
+func parseTopology(file string) *topo.Topology {
+	b, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return topo.NewTopologyYAML(b)
+}
+
 // Run is the main entry point
 func Run() {
+	// parse command line arguments
+	flag.Parse()
+	command := flag.Arg(0)
+	file := flag.Arg(1)
+
+	// handle command
+	switch command {
+	case "start":
+		t := parseTopology(file)
+		d := deploy.NewDeploy(t)
+		d.Start()
+	case "stop":
+		t := parseTopology(file)
+		d := deploy.NewDeploy(t)
+		d.Stop()
+	default:
+		log.Fatal("unknown command: ", command)
+	}
 }
